0c31bcf3946443ce9385ab15f6a2cf4763eb7603
AntonManzar <103017216+AntonManzar@users.noreply.github.com>
2022-05-18 22:29:22 +0300

    Initial commit

---
+++ src/pack/Main.java
@@ -0,0 +1,10 @@
+package pack;
+
+
+public class Main {
+    public static void main(String[] args) {
+        if(Math.pow(Math.E,Math.PI) < Math.pow(Math.PI,Math.E)) System.out.println("<");
+        else if (Math.pow(Math.E,Math.PI) > Math.pow(Math.PI,Math.E)) System.out.println(">");
+        else System.out.println("=");
+    }
+}
